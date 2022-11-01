f15c15d72a378a0c21a9a95b4abb20f3b047b503
nnastaran83 <nnastaran83@gmail.com>
2022-11-01 14:58:02 +0200

    started Translate

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,36 +1,43 @@
 import React, { useEffect, useState, useRef } from "react";
 
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
     const ref = useRef();
+
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
             return null;
         }
-
         return (
             <div className="item" key={option.value} onClick={() => onSelectedChange(option)}>
-
                 {option.label}
             </div>
         );
     });
 
+
     useEffect(() => {
-        document.body.addEventListener('click', (event) => {
+        const onBodyClick = (event) => {
             if(ref.current.contains(event.target)){
                 return;
             }
             setOpen(false);
-        });
+        };
+
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
     }, []);
+
 
 
     return (
         <div ref={ref} className="ui form">
             <div className="ui field">
-                <label className="label">Select a Color</label>
+                <label className="label">{label}</label>
                 <div className={`ui labeled icon dropdown ${open ? 'visible active' : ''} selection`}
                     onClick={() => setOpen(!open)}>
                     <i className="dropdown icon"></i>
@@ -40,10 +47,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
                     </div>
                 </div>
             </div>
-
-
         </div>
-
     );
 };
 
